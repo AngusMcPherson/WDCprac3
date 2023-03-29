7352bb91c69935c6ae5b049f5595961fd0eae793
AngusMcPherson <110000587+AngusMcPherson@users.noreply.github.com>
2023-03-29 18:29:44 +1030

    Add files via upload

--- task3.js
+++ task3.js
@@ -1,6 +1,28 @@
+const blueDiv = document.getElementById('mcount');
+const postDiv = document.getElementById('posts');
 var counter = 0;
 
-function onMouseOver() {
-  counter += 1;
-  document.getElementById("mcount").innerText = counter;
-}
+blueDiv.addEventListener('mouseenter', function (event) {
+  counter++;
+  blueDiv.innerText = counter;
+});
+
+
+var myButton = document.getElementById("poster")
+myButton.addEventListener('click', postDateContent);
+function postDateContent() {
+    console.log("im being called now");
+    const content = document.getElementById('textInput').value;
+    const individualPost = document.createElement("div");
+    const dateOfPost = document.createElement("p");
+    dateOfPost.className = 'post-time';
+    dateOfPost.innerText = new Date();
+    dateOfPost.style.color = "grey";
+    dateOfPost.style.fontWeight = "bold";
+    individualPost.appendChild(dateOfPost);
+    const postContent = document.createElement("p");
+    postContent.className = 'post-content';
+    postContent.innerText = content;
+    individualPost.appendChild(postContent);
+    postDiv.appendChild(individualPost)
+  }
